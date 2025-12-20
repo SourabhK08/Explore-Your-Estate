df9a68e28cefad21d690a0cc6ec2f2c734570100
Sourabhk08 <kakanisourabh23@gmail.com>
2025-12-20 14:01:50 +0530

    project setup complete

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-[#0a4c84] hover:text-[#e6e6e6]">EYE - Explore Your Estate</h1>
+    </div>
+  );
+}
